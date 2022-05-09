01bbb826121ec83e6572c334dd7246e602c254ba
Mario Rojas <mariro_ch@hotmail.com>
2022-05-09 20:34:17 +0200

    First Commit

---
+++ scripts/constants.py
@@ -0,0 +1,5 @@
+__author__ = "Mario Rojas"
+__license__ = "MIT"
+__version__ = "0.1.0"
+__maintainer__ = "Mario Rojas"
+__status__ = "Dev"
---
+++ scripts/helpers.py
@@ -0,0 +1,7 @@
+#!/usr/bin python3
+
+__author__ = "Mario Rojas"
+__license__ = "MIT"
+__version__ = "0.1.0"
+__maintainer__ = "Mario Rojas"
+__status__ = "Dev"
