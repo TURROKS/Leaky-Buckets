2c823efc1502e87768db6f08cf5fae7832ef491e
Mario Rojas <mariro_ch@hotmail.com>
2022-05-09 21:00:33 +0200

    Created get_bucket function

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -1,7 +1,15 @@
 #!/usr/bin python3
+import requests
+from scripts.constants import BASE_URI
 
 __author__ = "Mario Rojas"
 __license__ = "MIT"
 __version__ = "0.1.0"
 __maintainer__ = "Mario Rojas"
 __status__ = "Dev"
+
+
+def get_buckets(apikey, keyword, limit):
+
+    data = requests.get(BASE_URI+"0/{}?access_token={}&keywords={}".format(limit, apikey, keyword))
+    print(data.json())
