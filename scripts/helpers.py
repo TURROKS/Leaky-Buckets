c9b79a4d154dc22bdd9b88cb72611a23ca842763
Mario Rojas <mariro_ch@hotmail.com>
2022-05-21 14:15:03 +0200

    updated get_bucket_by_id_keyword returns

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -12,18 +12,21 @@ __status__ = "Dev"
 def get_buckets_all(apikey, limit, offset):
     """ Returns all buckets found, accepts a limit argument """
     data = requests.get(BASE_URI + "s/{}/{}?access_token={}".format(offset, limit, apikey))
+    print("all")
     print(data.json())
 
 
 def get_buckets_with_keyword(apikey, keyword, limit, offset):
     """ Returns buckets based on the input keyword, accepts a limit argument """
     data = requests.get(BASE_URI+"s/{}/{}?access_token={}&keywords={}".format(offset, limit, apikey, keyword))
+    print("bucket by keyword")
     print(data.json())
 
 
 def get_bucket_by_id(apikey, bucket_id, limit, offset):
     """ Returns contents of a bucket based on its ID """
     data = requests.get(BASE_URI + "/{}/files/{}/{}?access_token={}".format(bucket_id, offset, limit, apikey))
+    print("bucket by id")
     print(data.json())
 
 
@@ -31,4 +34,7 @@ def get_bucket_by_id_keyword(apikey, bucket_id, limit, offset, keyword):
     """ Returns contents of a bucket based on its ID """
     data = requests.get(BASE_URI + "/{}/files/{}/{}?access_token={}&keywords=".format(bucket_id, offset, limit, apikey,
                                                                                       keyword))
-    print(data.json())
+    parsed = data.json()
+    print("buckets id keyword")
+    for file in parsed['files']:
+        print(file['filename'])
