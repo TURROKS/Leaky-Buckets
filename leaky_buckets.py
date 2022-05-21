391fbc2feb8c5f92a1632f65e43b3a13a49e2dea
Mario Rojas <mariro_ch@hotmail.com>
2022-05-21 13:20:38 +0200

    added new arguments

--- leaky_buckets.py
+++ leaky_buckets.py
@@ -5,16 +5,17 @@ import scripts.helpers as funcs
 
 __author__ = "Mario Rojas"
 __license__ = "MIT"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __maintainer__ = "Mario Rojas"
 __status__ = "Dev"
 
 
 parser = argparse.ArgumentParser(description="This Tool leverages Gray Hat Warfare to look for exposed storage buckets",
                                  epilog='Enjoy the tool')
-parser.add_argument('-a', '--api', type=str, default='', help='Enter your API Key')
-parser.add_argument('-k', '--keyword', default="mp3", help="Keyword to search")
+parser.add_argument('-a', '--api', type=str, required=True)
+parser.add_argument('-k', '--keyword', type=str, help="Keyword to search")
 parser.add_argument('-l', '--limit', default=100, help="Limit results")
+parser.add_argument('-s', '--start', default=0, type=int, help="Buckets Offset")
 
 # Global Arguments
 args = parser.parse_args()
@@ -22,8 +23,13 @@ args = parser.parse_args()
 
 def main():
 
+    """ Confirm API """
     if args.api:
-        funcs.get_buckets(args.api, args.keyword, args.limit)
+        """ Select Function """
+        if args.keyword:
+            funcs.get_buckets_with_keyword(args.api, args.keyword, args.limit)
+        else:
+            funcs.get_buckets_all(args.api, args.limit, args.start)
 
 
 if __name__ == '__main__':
